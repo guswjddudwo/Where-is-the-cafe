5617b3f8e9b6b6a4f5bd770deb6a69fd35e794ff
guswjddudwo <guswjddudwo@gmail.com>
2022-12-27 17:42:13 +0900

    리뷰 이미지추가 리뷰섹션 구조 추가

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // 카페 소개 섹션 수평 슬라이드 기능
-new Swiper(".cafe__place .swiper", {
+new Swiper(".cafe__place .place", {
   // Optional parameters
   direction: "horizontal", // 수평 슬라이드(기본값)
   loop: true, // 반복 재생 여부
@@ -16,9 +16,23 @@ new Swiper(".cafe__place .swiper", {
     el: ".cafe__place .swiper-pagination", // 페이지 번호 요소
     clickable: true, // 사용자의 페이지 번호 제어 여부
   },
-  // navigation: {
-  //   // 이전/다음 슬라이드 버튼 사용
-  //   nextEl: ".cafe__place .swiper-button-next",
-  //   prevEl: ".cafe__place .swiper-button-prev",
-  // },
+});
+// 리뷰 섹션 슬라이드
+new Swiper(".cafe__place .reviw", {
+  // Optional parameters
+  direction: "horizontal", // 수평 슬라이드(기본값)
+  loop: true, // 반복 재생 여부
+  autoplay: {
+    // 자동 재생 여부
+
+    delay: 6000, // 5초마다 슬라이드 바뀜
+  },
+  slidesPerView: 4, // 한 번에 보여줄 슬라이드 개수
+  spaceBetween: 0, // 슬라이드 사이 여백(간격) px
+  //centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+  pagination: {
+    // 페이지 번호 사용
+    el: ".cafe__place .swiper-pagination", // 페이지 번호 요소
+    clickable: true, // 사용자의 페이지 번호 제어 여부
+  },
 });
