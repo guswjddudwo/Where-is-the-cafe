e5b64608e9207c618961e737c4a8ba1f12950545
guswjddudwo <guswjddudwo@gmail.com>
2022-12-29 18:24:04 +0900

    js 수정

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ new Swiper('.cafe__contents .swiper', {
   direction: 'horizontal', // 수평 슬라이드(기본값)
   loop: true, // 반복 재생 여부
   autoplay: { // 자동 재생 여부
-    delay: 5000 // 5초마다 슬라이드 바뀜
+    delay: 5000, // 5초마다 슬라이드 바뀜
+    pauseOnMouseEnter: true,
   },
   slidesPerView: 1, // 한 번에 보여줄 슬라이드 개수  
 });
