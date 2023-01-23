40b35dd23bcbb55b864c4c960917efe2c0aeb0a9
guswjdl1234 <guswjddudwo@gmail.com>
2023-01-23 23:20:27 +0900

    ,

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ new Swiper(".cafe__contents .swiper", {
     // 자동 재생 여부
     delay: 3000, // 초마다 슬라이드 바뀜
     pauseOnMouseEnter: true,
-    // spaceBetween: 100,
+    spaceBetween: 10,
   },
   slidesPerView: 1, // 한 번에 보여줄 슬라이드 개수
   navigation: {
